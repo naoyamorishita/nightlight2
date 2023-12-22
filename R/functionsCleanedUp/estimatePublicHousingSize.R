0b03360fcdd4d4168b28562dc26a9205fde4242f
naoya_morishita <0zh4772g325515u64@gmail.com>
2023-12-22 00:34:10 -0500

    Corrected public housing area estimation!

--- R/functionsCleanedUp/estimatePublicHousingSize.R
+++ R/functionsCleanedUp/estimatePublicHousingSize.R
@@ -45,3 +45,62 @@ estimatePublicHousingSize <- function(
     st_write()
 
 }
+
+setwd("/Volumes/volume 1/GIS Projects/nightlight/nightlight2/miami")
+b <- st_read("miami_boundary_3086.geojson") %>%
+  st_as_sf()
+
+ph <- st_read("miami_ph.geojson") %>%
+  st_as_sf() %>%
+  st_transform(crs = st_crs(b)) %>%
+  st_intersection(b)
+
+rst <- raster::raster("alanYearlyMean_miami.tif")
+
+r <- rst %>%
+  rasterToPolygons(.) %>%
+  st_as_sf() %>%
+  mutate(gridID = 1:nrow(.))
+
+phg <- st_join(ph, r, st_intersects)
+
+gridIDUnique <- unique(phg$ gridID)
+
+gridPh <- r[r$gridID %in% gridIDUnique,]
+gc()
+
+bld <- st_read("miami_bld.geojson") %>%
+  st_as_sf() %>%
+  st_transform(st_crs(b))
+
+bldg <- st_join(bld,
+               gridPh,
+               st_intersects,
+               left = FALSE) %>%
+  mutate(bldArea = st_area(.) %>%
+           as.numeric(.))
+
+rm(bld)
+gc()
+
+phbld <- st_join(ph,
+                 bldg,
+                 st_nearest_feature)
+
+
+gph <-
+bldg %>%
+  st_drop_geometry() %>%
+  group_by(gridID) %>%
+  summarize(sumPhArea = sum(bldArea))
+
+g <- left_join(r,
+               gph,
+               "gridID")
+g[is.na(g)] <- 0
+
+g %>%
+  rasterize(rst,
+            field = "sumPhArea") %>%
+  writeRaster("phbld.tif",
+              overwrite = T)
