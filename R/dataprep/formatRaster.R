0f43d98950ff91a5ad7c8f634650f497d3f50aa7
naoya_morishita <0zh4772g325515u64@gmail.com>
2023-12-21 23:06:33 -0500

    Generalized functions!

--- R/dataprep/formatRaster.R
+++ R/dataprep/formatRaster.R
@@ -5,20 +5,64 @@ library(tidyr)
 
 setwd("/Volumes/volume 1/GIS Projects/nightlight/nightlight2/miami")
 
-boundary <- st_read("miami_boundary_3086.geojson") %>% 
-  st_as_sf()
+# boundary <- st_read("miami_boundary_3086.geojson") %>% 
+#   st_as_sf()
+# 
+# alan <- raster::raster("miami_nightlihgt_yearly/h09v06_meanAlan.tif")
+# 
+# boundary_4326 <- st_transform(boundary, "epsg: 4326")
+# 
+# alan <- alan %>% 
+#   crop(boundary_4326) %>% 
+#   mask(boundary_4326)
+# 
+# alan <- projectRaster(alan, 
+#                       crs = crs(boundary))
+# 
+# writeRaster(alan,
+#             "alanYearlyMean_miami.tif",
+#             overwrite = T)
 
-alan <- raster::raster("miami_nightlihgt_yearly/h09v06_meanAlan.tif")
+formatAlan <- function(
+    pathToAlan,
+    pathToboundary,
+    outPath
+){
+  b <- st_read(pathToboundary)
+  b4326 <- st_transform(boundary, 
+                        "epsg: 4326")
+  
+  alan <- raster::raster(pathToAlan) %>% 
+    crop(b4326) %>% 
+    mask(b4326) %>% 
+    projectRaster(crs = crs(b))
+  
+  writeRaster(alan,
+              outPath,
+              overwrite = T)
+}
 
-boundary_4326 <- st_transform(boundary, "epsg: 4326")
+formatRasterToAlan <- function(
+    pathToRaster,
+    pathToAlan,
+    outPath
+){
+  r <- raster::raster(pathToRaster)
+  alan <- raster::raster(pathToAlan)
+  
+  r <-
+  r %>% 
+    projectRaster(crs = crs(alan)) %>% 
+    resample(y = alan)
+  
+  print(r)
+  plot(r)
+  
+  writeRaster(r,
+              outPath,
+              overwrite = T)
+}
 
-alan <- alan %>% 
-  crop(boundary_4326) %>% 
-  mask(boundary_4326)
-
-alan <- projectRaster(alan, 
-                      crs = crs(boundary))
-
-writeRaster(alan,
-            "alanYearlyMean_miami.tif",
-            overwrite = T)
+formatRasterToAlan("miami_ndvi.tif",
+                   "alanYearlyMean_miami.tif",
+                   "ndvi_coarsened.tif")
